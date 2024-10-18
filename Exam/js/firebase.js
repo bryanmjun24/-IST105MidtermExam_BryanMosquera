505a8f301864d5c577bfea90e67423c375ee3115
Bryan <mosquera_31_1a@yahoo.es>
2024-10-17 20:40:37 -0700

    first branch

---
+++ Exam/js/firebase.js
@@ -0,0 +1,24 @@
+/*
+  CCTB Website Development
+  IST105
+  Oct 2024
+  Description: This is a simple login website where students are asked to 
+  implement Social Network Login with Firebase
+*/
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBc-fxoVss-AdBSLc1enD6uQUwgRrVaFPY",
+  authDomain: "ist105midtermexam-bryanm.firebaseapp.com",
+  projectId: "ist105midtermexam-bryanm",
+  storageBucket: "ist105midtermexam-bryanm.appspot.com",
+  messagingSenderId: "1024449543381",
+  appId: "1:1024449543381:web:d8a04cb8fc6132e9cc765c"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
